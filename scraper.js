d1223a4f585c2f26454300d76f0073bce2386414
borislavstoychev <stoy4ew@gmail.com>
2021-12-23 15:51:25 +0200

    final solution

--- scraper.js
+++ scraper.js
@@ -3,40 +3,49 @@ const saveData = require('./saveData');
 
 
 async function scrapeProduct(url){
+    // request to load the page
     const browser = await puppeteer.launch()
     const page = await browser.newPage();
-    await page.goto(url)
-
-    const [el] = await page.$x('//html/body/div[5]/div/form/div[2]/div[1]/main/div/div[3]/div[1]/div[1]/h1');
-    const txt = await el.getProperty('textContent');
-    const name = await txt.jsonValue();
-
-    const [el2] = await page.$x('//html/body/div[5]/div/form/div[2]/div[1]/main/div/div[3]/div[2]/div[2]/span');
-    const txt2 = await el2.getProperty('textContent');
-    const color = await txt2.jsonValue();
-
-    const [el3] = await page.$x('//html/body/div[5]/div/form/div[2]/div[1]/main/div/div[3]/div[1]/div[2]/span[4]');
-    const txt3 = await el3.getProperty('textContent');
-    let price = await txt3.jsonValue();
+    await page.goto(url);
+    // collect name
+    const name = await page
+    .$eval(
+        'h1.product-name', 
+        el => el.textContent
+        );
+    // collect color
+    const color = await page
+    .$eval(
+        'div.colors-info > span.colors-info-name', 
+        el2 => el2.textContent
+        );
+    // collect price
+    let price = await page
+    .$eval(
+        'div.product-prices > span.product-sale.product-sale--discount', 
+        el3 => el3.textContent
+        );
     price =  Number(price.slice(1));
-
-    const [el4] = await page.$x('//html/body/div[5]/div/form/div[2]/div[1]/main/div/div[3]/div[3]/form/div[2]/div/div');
-    const txt4 = await el4.getProperty('textContent');
-    let result = await txt4.jsonValue();
+    // collect size
     const size = [];    
-    result.match(/(XS|S|M|L|\d?XL)(\s-\s\w+\s\w+)?/gm)
-    .forEach(element => {
-        const [sizes, availability] = element.split(' - ')
+    const sizeOptions = await page
+    .$$eval(
+        '#sizeSelector > div > span', 
+        options => { return options.map(option => option.innerText) }
+        );
+    sizeOptions.forEach(option => {
+        const [sizes, availability] = option.split(' - ');
         const obj = {};
         if (sizes){
-            availability !== undefined ? obj[sizes] = availability : obj[sizes] = "Available"
-            size.push(obj)    
+            availability !== undefined 
+            ? obj[sizes] = availability 
+            : obj[sizes] = "Available";
+            size.push(obj);  
         };
-    });
-
+    });    
+    // output the data as json file
     saveData({name, color, price, size}, 'edited.json');
     browser.close();
 };
 
-
-scrapeProduct('https://shop.mango.com/gb/women/skirts-midi/midi-satin-skirt_17042020.html?c=99')
+scrapeProduct('https://shop.mango.com/gb/women/skirts-midi/midi-satin-skirt_17042020.html?c=99');
